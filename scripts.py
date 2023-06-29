a646118888f6d8374a3f78cd20d95b73d3be713e
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-06-29 18:59:35 +0300

    Убрал название предмета в отдельную переменную,  урок теперь достается из предмета и также дата этого урока

--- scripts.py
+++ scripts.py
@@ -1,6 +1,6 @@
 import random
 
-from datacenter.models import Mark, Chastisement, Schoolkid, Commendation, Subject, Teacher
+from datacenter.models import Mark, Chastisement, Schoolkid, Commendation, Subject, Teacher, Lesson
 
 
 def fix_marks(schoolkid):
@@ -11,20 +11,24 @@ def remove_chastisements(schoolkid):
     Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(schoolkid):
+def create_commendation(schoolkid, subject_name):
     year_of_study = schoolkid.year_of_study
-    subject = Subject.objects.filter(title='Музыка', year_of_study=year_of_study)  # В title ввести название предмета
+    subject = Subject.objects.filter(title=subject_name, year_of_study=year_of_study)
+    subject = random.choice(subject[:])
     teacher = Teacher.objects.all()
     teacher = random.choice(teacher[:])
+    lesson = Lesson.objects.filter(year_of_study=year_of_study, subject=subject)
+    lesson_date = random.choice(lesson[:]).date
 
     Commendation.objects.create(text='Молодец!', schoolkid=schoolkid, teacher=teacher, subject=subject,
-                                created='2018-09-15')  # Введите дату в created для добавления похвалы
+                                created=lesson_date)
 
 
 if __name__ == '__main__':
     name = 'Фролов Иван Григорьевич'
+    subject_name = 'Музыка'
     schoolkid = Schoolkid.objects.filter(full_name__contains=name).first()
 
     fix_marks(schoolkid)  # Изменяем оценки 2 и 3 на 5
     remove_chastisements(schoolkid)  # Удаляем замечания от учителей
-    create_commendation(schoolkid)  # Добавляем похвалу для ученика
+    create_commendation(schoolkid, subject_name)  # Добавляем похвалу для ученика
