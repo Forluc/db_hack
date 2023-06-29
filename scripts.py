c806cadde4673ac77ed05a2a41af7cf583273b6c
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-06-29 10:39:16 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -4,43 +4,27 @@ from datacenter.models import Mark, Chastisement, Schoolkid, Commendation, Subje
 
 
 def fix_marks(schoolkid):
-    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    for mark in marks:
-        mark.points = 5
-        mark.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    for chastisement in chastisements:
-        chastisement.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(name):
-    schoolkid = Schoolkid.objects.filter(full_name__contains=name)
-    try:
-        schoolkid = schoolkid[1]
-        return False
-    except:
-        if schoolkid[0]:
-            schoolkid = schoolkid[0]
-            year_of_study = schoolkid.year_of_study
-            subjects = Subject.objects.filter(year_of_study=year_of_study)
-            teacher = Teacher.objects.all()
-            teacher = random.choice(teacher[:])
-            date = random.choice(['2018-09-15', '2018-09-20', '2018-09-21', '2018-09-22', '2018-09-23'])
-            text = random.choice(['Молодец', 'Умничка', 'Молоток', 'Все сделал правильно', '5+'])
+def create_commendation(schoolkid):
+    year_of_study = schoolkid.year_of_study
+    subject = Subject.objects.filter(title='Музыка', year_of_study=year_of_study)  # В title ввести название предмета
+    teacher = Teacher.objects.all()
+    teacher = random.choice(teacher[:])
 
-            for subject in subjects:
-                Commendation.objects.create(text=text, schoolkid=schoolkid, teacher=teacher, subject=subject,
-                                            created=date)
-            return True
-        else:
-            return False
+    Commendation.objects.create(text='Молодец!', schoolkid=schoolkid, teacher=teacher, subject=subject,
+                                created='2018-09-15')  # Введите дату в created для добавления похвалы
 
 
 if __name__ == '__main__':
-    if create_commendation('Фролов Иван Григорьевич'):
-        print('Записи созданы')
-    else:
-        print('Введите корректное уникальное ФИО')
+    name = 'Фролов Иван Григорьевич'
+    schoolkid = Schoolkid.objects.filter(full_name__contains=name).first()
+
+    fix_marks(schoolkid)  # Изменяем оценки 2 и 3 на 5
+    remove_chastisements(schoolkid)  # Удаляем замечания от учителей
+    create_commendation(schoolkid)  # Добавляем похвалу для ученика
